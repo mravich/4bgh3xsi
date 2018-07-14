dd31a5622c7cc3a44ec68f1cbe122216cabd6526
LightzOut <mpaleka@gmail.com>
2018-07-14 21:37:24 +0200

    message

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 import axios, {get} from 'axios'
 
+
 class App extends Component {
 
   goToApi(){
     const url = "http://localhost:8000"
     const proxyUrl = "https://cors-anywhere.herokuapp.com/"
    fetch(url, {
-     headers:{'Access-Control-Allow-Origin':'*'}}).catch(console.log)
+     headers:{'Access-Control-Allow-Origin':'*'}}).then((res)=>{console.log(res)})
+     .catch(console.log)
+  }
+
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Activity je poslan: ' + this.state.value);
   }
 
   render() {
     return (
       <div className="App">
-      <button onClick={()=>this.goToApi()}>
-      Go to API!
-      </button>
+      <form onSubmit={this.handleSubmit}>
+       <label>
+         Add Activity
+         <input type="text" value={this.state.value} onChange={this.handleChange} />
+       </label>
+       <input type="submit" value="Submit" />
+     </form>
       </div>
     );
   }
