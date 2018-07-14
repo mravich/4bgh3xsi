cd26c1081ad835b3b811c9f7d1bc25dbf011ac21
LightzOut <mpaleka@gmail.com>
2018-07-14 20:43:01 +0200

    message

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios, {get} from 'axios'
 
 class App extends Component {
+
+  goToApi(){
+    const url = "https://localhost:8000"
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+    get(proxyUrl + url,
+    {
+    //  withCredentials: true,
+    mode:"no-cors",
+      headers:{
+        "Content-Type": "text/plane",
+        "Accept": "application/json"
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <button onClick={()=>this.goToApi()}>
+      Go to API!
+      </button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Axios from 'axios'
 
+//Axios.defaults.withCredentials = true
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
