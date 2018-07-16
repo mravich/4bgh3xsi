c2f4076613cf6199e5a16653bc1d28f7c8738e5a
mravich <pistolmrav@gmail.com>
2018-07-16 22:32:23 +0200

    Added collection rendering and function for api request

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import { get, post } from 'axios';
 import Table from './components/table';
-import {DATABASE_COLLECTIONS} from "./constants";
+import { DATABASE_COLLECTIONS } from './constants';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       value: '',
       message: '',
+      showMenu: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.showMenu = this.showMenu.bind(this);
   }
 
-
+  showMenu(event) {
+    event.preventDefault();
+    this.setState({
+      showMenu: true,
+    });
+  }
 
   handleChange(event) {
     this.setState({ value: event.target.value });
@@ -23,16 +30,27 @@ class App extends Component {
     alert('Activity je poslan: ' + this.state.value);
   }
 
+  /*OVO TU TI JE IZLIST SVIH TABLICA -> SVAKA MORA DOBIT property="Ime koleckije" da bi znala koje fieldove treba imati -> to je u database.js
+
+
+        <h1>DATABASE MANAGMENT APP</h1>
+        {Object.entries(DATABASE_COLLECTIONS).map((item, key) => {
+        console.log("ITEM: ", item)
+          return <Table property={item[0]} key={key} />;
+        })}
+   */
   render() {
     return (
       <div
         className="container"
         style={{ textAlign: 'center', maxWidth: '1900px' }}
       >
-        <h1>DATABASE MANAGMENT APP</h1>
-          {Object.entries(DATABASE_COLLECTIONS).map((item,key)=>{
-            return <Table property={item[0]} key={key}/>
-          })}
+        <button onClick={this.showMenu}>Show me databases</button>
+        {this.state.showMenu ? (
+          <div className="menu">
+            <Table property="Activity" />
+          </div>
+        ) : null}
       </div>
     );
   }
